8005b2f0395593ec38c9ac0ffd15e0bb1eec9b57
Alan Rivera <arivera@bsd220.org>
2023-11-27 17:50:17 -0600

    Initial Commit

---
+++ Hostr/HostrApp.swift
@@ -0,0 +1,17 @@
+//
+//  HostrApp.swift
+//  Hostr
+//
+//  Created by Alan Rivera on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct HostrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
