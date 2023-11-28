667abbfbf164834bbc045b4be7c6b49b212728f6
Alan Rivera <arivera@bsd220.org>
2023-11-27 23:04:50 -0600

    Made Title Screen

--- Hostr/ContentView.swift
+++ Hostr/ContentView.swift
@@ -9,16 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            ZStack{
+                Color.blue.opacity(1.0).ignoresSafeArea()
+                VStack {
+                    Text("Hostr")
+                        .font(Font.custom("Marker Felt", size: 45))
+                    Spacer()
+                    Image("IMG_0695").resizable().frame(width: 300, height: 300, alignment: .center)
+                    Spacer()
+                    NavigationLink(destination : HostSelecter()) {
+                        Text("Click Me")
+                            .font(.title).bold()
+                            .frame(width: 300, height: 150, alignment: .center)
+                            .background(Color.gray)
+                            .foregroundColor(Color.black)
+                            .cornerRadius(50)
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
---
+++ Hostr/HostSelecter.swift
@@ -0,0 +1,22 @@
+//
+//  HostSelecter.swift
+//  Hostr
+//
+//  Created by Alan Rivera on 11/27/23.
+//
+
+import SwiftUI
+
+struct HostSelecter: View {
+    var body: some View {
+        VStack {
+            Rectangle()
+                .frame(height: 1)
+        }
+    }
+}
+struct HostSelecter_Previews: PreviewProvider {
+    static var previews: some View {
+        HostSelecter()
+    }
+}
